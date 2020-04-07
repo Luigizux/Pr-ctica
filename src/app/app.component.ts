7dd895452f52c25a6348c34ebac6d76c7ddfff87
Luigizux <luiszuniga1995@gmail.com>
2020-04-07 16:55:16 -0400

    Quinto commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'Calculadora';
   Resultado:number = 0.0;
+  Nro1: number = 0.0;
+  Nro2: number = 0.0;
+  Operatoria: number  = 0;
 
+  Operacion(Operatoria, Nro1, Nro2);
+  
   Operacion(Operatoria, Nro1, Nro2)
   {
     /*
@@ -18,38 +24,37 @@ export class AppComponent {
     4.- División
     */
    
-   var PrimerNumero = Number(Nro1.value);
-   var SegundoNumero = Number(Nro2.value);
-  
-   if(isNaN(PrimerNumero) || isNaN(SegundoNumero)) // Validar que solo se ingrsen daton numéricos.
-   {
-      alert("Asigne solo números por favor");
-   }
-   else
-   {
-    switch (Operatoria.value) 
+    var PrimerNumero = Number(Nro1);
+    var SegundoNumero = Number(Nro2);
+    
+    if(isNaN(PrimerNumero) || isNaN(SegundoNumero)) // Validar que solo se ingrsen daton numéricos.
     {
-      case "1":         
-          this.Resultado = (PrimerNumero + SegundoNumero);
-        break;
-      case "2":
-          this.Resultado = (PrimerNumero - SegundoNumero);
-        break;
-      case "3":
-          this.Resultado = (PrimerNumero * SegundoNumero);
-        break;
-      case "4":
-          this.Resultado = (PrimerNumero / SegundoNumero);
-        break;
-      default:
-          alert("Escoja una operación válida");
-        break;
+        alert("Asigne solo números por favor");
     }
-   }
-    /*console.log(Operatoria.value);
-    console.log(PrimerNumero);
-    console.log(SegundoNumero);
-    */
-   return false;
+    else
+    {
+      switch (Operatoria.value) 
+      {
+        case "1":         
+            this.Resultado = (PrimerNumero + SegundoNumero);
+          break;
+        case "2":
+            this.Resultado = (PrimerNumero - SegundoNumero);
+          break;
+        case "3":
+            this.Resultado = (PrimerNumero * SegundoNumero);
+          break;
+        case "4":
+            this.Resultado = (PrimerNumero / SegundoNumero);
+          break;
+        default:
+            alert("Escoja una operación válida");
+          break;
+      }
+    }
+      console.log(Operatoria.value);
+      console.log(PrimerNumero);
+      console.log(SegundoNumero);
+      return false;
   }
 }
